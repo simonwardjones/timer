1963c38ab55ca8f7c4f783953bd6a32f3dc80883
simon.ward-jones <simon.ward-jones@beamly.com>
2018-02-14 17:16:21 +0000

    extracting class

--- timer_click.py
+++ timer_click.py
@@ -1,12 +1,17 @@
+""" 
+Section:
+    click
+
+Author:
+    Simon Ward-Jones
+
+Description:
+    Count Down timer command line Tool
 """
-Count Down timer command line Tool
-"""
-import os 
-import sys
-from time import sleep
 import time
 import click
-# good old fasioned stop watch
+from timer import Timer
+
 
 @click.command(help="Exit the time with ctrl + c")
 @click.option('--countdown',type=int,help="Convert the timer to countdown with given seconds")
@@ -40,58 +45,5 @@ def cli(countdown=None):
         print(timer.get_time_hhmmss(timer.get_time()))
         sys.exit()
 
-
-
-class Timer:
-    def __init__(self):
-        self.start = time.time()
-        self.end = time.time()
-        self._is_running = False
-
-    def start_timer(self):
-        """restarts the time
-        """
-        self.start = time.time()
-        self._is_running = True
-
-
-    def restart(self):
-        """restarts the time
-        """
-        self.end = self.start = time.time()
-
-    def stop(self):
-        """stop the current time
-        """
-        if self._is_running:
-            self.end = time.time()
-        else:
-            print("Can't Stop until you start")
-
-    def get_time(self):
-        '''
-        Returns the time elapsed (Does not stop the counter).
-
-        Returns:
-            TYPE: string
-        '''
-        return self.end - self.start
-
-    def get_elapsed(self):
-        '''
-        Returns the time elapsed (Does not stop the counter).
-
-        Returns:
-            TYPE: string
-        '''
-        return time.time() - self.start
-
-    def get_time_hhmmss(self,duration):
-        """Returns the duration in HH:mm:ss (Does not reset the counter).
-        
-        Returns:
-            TYPE: Description
-        """
-        m, s = divmod(duration, 60) 
-        h, m = divmod(m, 60)
-        return f'{int(h):0>2}:{int(m):0>2}:{s:05.4f}'
+if __name__ == '__main__':
+    cli()
