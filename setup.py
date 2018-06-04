74692d6c8e812195de7df12032a68924ed7191ea
simonwardjones <simon.ward-jones@beamly.com>
2018-06-04 08:50:37 +0100

    PEP

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 from setuptools import setup
 
 setup(
-	name='Timer',
-	version='1.0.0',
-	py_modules='timer',
-	install_requires=['Click'],
-	entry_points={
-		'console_scripts':[
-			'timer=timer_click:cli'
-		]
-	},
+    name='Timer',
+    version='1.0.0',
+    py_modules='timer',
+    install_requires=['Click'],
+    entry_points={
+        'console_scripts': [
+            'timer=timer_click:cli'
+        ]
+    },
 )
-
